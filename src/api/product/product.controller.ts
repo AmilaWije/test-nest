3915e57b583d0d83dd18d44f0f7a555ae0967156
AmilaWije <eranga1995@gmail.com>
2026-02-11 00:11:14 +0530

    create functionality completed

--- src/api/product/product.controller.ts
+++ src/api/product/product.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { ProductService } from './product.service';
+import type { Iproduct } from './product.service';
 
-@Controller()
+@Controller('products')
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
 
-  @Get('all-products')
+  @Get()
   allProducts() {
     return this.productService.getAllProducts();
+  }
+
+  @Post('create')
+  createProduct(@Body() newProductData: Iproduct) {
+    return this.productService.createProduct(newProductData);
   }
 }
